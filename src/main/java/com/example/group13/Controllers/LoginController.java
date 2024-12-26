b61b9459a717265c821c011a4ed9074c763aac6f
baris can <bariscanaslan@outlook.com>
2024-12-26 20:42:35 +0300

    first-update

--- src/main/java/com/example/group13/Controllers/LoginController.java
+++ src/main/java/com/example/group13/Controllers/LoginController.java
@@ -1,4 +1,22 @@
 package com.example.group13.Controllers;
 
-public class LoginController {
+import com.example.group13.Models.Model;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+    public Button admin_btn;
+    public Button manager_btn;
+    public Button cashier_btn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        admin_btn.setOnAction(event -> Model.getInstance().getViewManager().showAdminWindow());
+        manager_btn.setOnAction(event -> Model.getInstance().getViewManager().showManagerWindow());
+        cashier_btn.setOnAction(event -> Model.getInstance().getViewManager().showCashierWindow());
+    }
 }
